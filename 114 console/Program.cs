c26426f4cbb3f7f990afc99cb22eab6f6def2c47
306 <306@306NEW-7>
2023-07-05 11:21:27 +0300

    Добавьте файлы проекта.

---
+++ 114 console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MyLibrary;
+
+namespace ConsoleApp//Студент Корнилов Артем
+{
+    class Program
+    {
+        static void Main(string[] args)//Студент Корнилов Артем
+        {
+            Person person = new Person("123 Основная улица");//Студент Корнилов Артем
+
+            person.AddHobby(new Hobby("Чтение", "Мне нравится читать книги"));
+            person.AddHobby(new Hobby("Спорт", "Я люблю заниматься спортом"));
+            person.AddHobby(new Hobby("Кулинария", "Мне нравится готовить вкусные блюда."));//Студент Корнилов Артем
+
+            person.ShowAll();
+
+            person.Hobbies.Sort();
+            person.ShowAll();
+
+            Console.ReadLine();//Студент Корнилов Артем
+        }
+    }
+}
